f903f06f562808380ef62eaf406c76204690f63e
Shivam Bansal <shivam5992@gmail.com>
2013-10-28 01:35:05 -0700

    source code retrived

---
+++ main.py
@@ -0,0 +1,15 @@
+#download the source code of a website
+import urllib.request
+import re
+
+urls = ["http://google.com","http://nytimes.com","http://CNN.com"]
+i = 0
+regex = b'<title>(.+?)</title>'
+pattern = re.compile(regex)
+
+while i < len(urls): 
+	htmlfile = urllib.request.urlopen(urls[i])
+	htmltext = htmlfile.read()
+	titles = re.findall(pattern,htmltext)
+	print (titles)
+	i += 1
