772c32359e2918fb2f0d308f8d728a12988a2c52
Shivam Bansal <shivam5992@gmail.com>
2013-10-29 01:57:02 -0700

    fixed symbol case

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ urls = ["http://google.com","http://nytimes.com","http://CNN.com"]
 i = 0
 regex = b'<title>(.+?)</title>'
 pattern = re.compile(regex)
-print(regex)
 
 while i < len(urls): 
 	htmlfile = urllib.request.urlopen(urls[i])
--- stock.py
+++ stock.py
@@ -1,19 +1,16 @@
 import urllib.request
 import re
 
-symbolslist = ["AAPL","SPY","GOOG","NFLX"]
+symbolslist = ["aapl","spy","goog","nflx"]
 i = 0
 while i < len(symbolslist):
 	url = "http://in.finance.yahoo.com/q?s=" + symbolslist[i] 
 	htmlfile = urllib.request.urlopen(url)
-	htmltext = htmlfile.read()
+	htmltext = htmlfile.read().decode('utf-8')
 
-	symbol = bytes(symbolslist[i], 'Utf-8')
-
-	regex = b'<span id="yfs_l84_'+ symbol + b'">(.+?)</span>'
-	print(regex)
+	regex = '<span id="yfs_l84_'+ symbolslist[i] + '">(.+?)</span>'
 	pattern = re.compile(regex)
 	price = re.findall(pattern,htmltext)
 
-	print ("the price of",symbolslist[i],"is",str(price))
+	print ("the price of",symbolslist[i],"is",price)
 	i += 1
