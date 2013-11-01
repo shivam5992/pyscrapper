dd5c9ab956d0a19fe03eba9fdeb58405f0bbc5f7
Shivam Bansal <shivam5992@gmail.com>
2013-10-31 23:54:00 -0700

    scrapping through networking

--- scrapgoogle.py
+++ scrapgoogle.py
@@ -1,21 +1,7 @@
 import urllib.request
 import re
 
-symbolfile = open("symbols1.txt")
-
-symbolslist = symbolfile.read()
-
-newsymbolslist = symbolslist.split("\n")
-
-i = 0
-while i < len(newsymbolslist):
-	url = "https://www.google.com/finance?q=" + newsymbolslist[i] 
-	htmlfile = urllib.request.urlopen(url)
-	htmltext = htmlfile.read().decode('utf-8')
-
-	regex = '<span id="ref_[^.]*_l" class="unchanged"></span>'
-	pattern = re.compile(regex)
-	price = re.findall(pattern,htmltext)
-
-	print ("the price of",newsymbolslist[i],"is",price)
-	i += 1
+url = "https://www.google.com/finance/getprices?q=AAPL&x=NASD&i=120&p=25m&f=c&df=cpct&auto=1&ts=1383288707413&ei=cJ9yUujREYnHkgWoIw"
+htmlfile = urllib.request.urlopen(url)
+htmltext = htmlfile.read().decode('utf-8').split()
+print (htmltext[len(htmltext)-1])
