2c28ca113b2871ac6fd58aff18eb5a9d1a01c32e
Shivam Bansal <shivam5992@gmail.com>
2013-12-25 16:42:29 +0530

    json and dict response

--- google_movies/show.py
+++ google_movies/show.py
@@ -1,5 +1,8 @@
 from BeautifulSoup import BeautifulSoup
 import re,urllib
+from pprint import pprint
+import json
+
 
 class google_movie_scrapper:
 	
@@ -10,23 +13,43 @@ class google_movie_scrapper:
 
 	def scrap(self):
 		soup_object = self.soup
-		resp = []
+		output = []
 		for theater in soup_object.findAll("div", {"class":re.compile("theater")}):
-			print "Theater:", theater.a.string
-			print "Theater Info:", theater.find("div", {"class":re.compile("info")}).contents[0]
+			resp = {}
+			resp['theater'] = theater.a.string
+			resp['info'] = theater.find("div", {"class":re.compile("info")}).contents[0]
+			#print "Theater:", theater.a.string
+			#print "Theater Info:", theater.find("div", {"class":re.compile("info")}).contents[0]
+			movie_list = []
 			for movie in theater.find("div", {"class":re.compile("showtimes")}).findAll("div", {"class":re.compile("movie")}):
-				print "Movie:", movie.a.string
-				print "Movie genere", re.sub('[^a-zA-Z/\n\.]', ' ', str(movie.span.text)).strip().replace("                 "," - ").replace("         Trailer","")
-				print "Movie Times:"
+				movie_dict = {}
+				movie_dict['movie_name'] = movie.a.string 
+				movie_dict['movie_genere'] = re.sub('[^a-zA-Z/\n\.]', ' ', str(movie.span.text)).strip().replace("                 "," - ").replace("         Trailer","")
+				#print "Movie:", movie.a.string
+				#print "Movie genere", re.sub('[^a-zA-Z/\n\.]', ' ', str(movie.span.text)).strip().replace("                 "," - ").replace("         Trailer","")
+				#print "Movie Times:"
+				showtimeslist = []
 				for time in movie.findAll("div",{"class":re.compile("times")})[0].findAll("span"):
 					time_string = re.sub('[^a-z0-9:\n\.]', ' ', str(time.text)).replace("8206","").replace("nbsp","").strip()
 					if time_string is not "":
-						print time_string
+						showtimeslist.append(time_string)
+						#print time_string
+				movie_dict['showtimes'] = showtimeslist
+				movie_list.append(movie_dict)
 
-			print "***"
+				resp['movie'] = movie_list
+			output.append(resp)
+		return output
 
-obj = google_movie_scrapper()
-obj.scrap()
+if __name__ == '__main__':
+	obj = google_movie_scrapper()
+	output = obj.scrap()
+	d = json.dumps(output)
+	f = open('respnse.json', 'w')
+	print >> f, d
+	f.close()
+	#print pprint(output)
+
 
 '''
 
